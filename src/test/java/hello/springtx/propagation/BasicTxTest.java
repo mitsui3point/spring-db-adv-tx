47d7b3b8d78e87841ce677f9fb8079a83d91f773
mitsui3point <rladbsgh0331@gmail.com>
2023-03-14 16:07:00 +0900

    feat($test.propagation): add - add: 스프링 트랜잭션 전파3, 4 - 전파 예제

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -48,6 +48,10 @@ public class BasicTxTest {
         log.info("트랜잭션 롤백 완료");
     }
 
+    /**
+     * 이번에는 트랜잭션이 각각 따로 사용되는 경우를 확인해보자.
+     * 이 예제는 트랜잭션1이 완전히 끝나고나서 트랜잭션2를 수행한다.
+     */
     @Test
     void double_commit() {
         log.info("트랜잭션1 시작");
@@ -72,5 +76,35 @@ public class BasicTxTest {
         TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션2 롤백");
         txManager.rollback(tx2);
+    }
+
+    /**
+     * - 외부 트랜잭션이 수행중인데, 내부 트랜잭션을 추가로 수행했다.
+     * - 외부 트랜잭션은 처음 수행된 트랜잭션이다. 이 경우 신규 트랜잭션( isNewTransaction=true )이 된다.
+     * - 내부 트랜잭션을 시작하는 시점에는 이미 외부 트랜잭션이 진행중인 상태이다. 이 경우 내부 트랜잭션은 외부 트랜잭션에 참여한다.
+     * - 트랜잭션 참여
+     *      내부 트랜잭션이 외부 트랜잭션에 참여한다는 뜻은 내부 트랜잭션이 외부 트랜잭션을 그대로 이어 받아서 따른다는 뜻이다.
+     *      다른 관점으로 보면 외부 트랜잭션의 범위가 내부 트랜잭션까지 넓어진다는 뜻이다.
+     *      외부에서 시작된 물리적인 트랜잭션의 범위가 내부 트랜잭션까지 넓어진다는 뜻이다.
+     *      정리하면 외부 트랜잭션과 내부 트랜잭션이 하나의 물리 트랜잭션으로 묶이는 것이다.
+     * - 내부 트랜잭션은 이미 진행중인 외부 트랜잭션에 참여한다. 이 경우 신규 트랜잭션이 아니다( isNewTransaction=false ).
+     * - 예제에서는 둘다 성공적으로 커밋했다.
+     */
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());//Participating in existing transaction
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);//아무것도 안함
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);//실제 물리 transaction commit(outer.isNewTransaction(); 신규 트랜잭션 -> true 이어야 물리적 commit 반영)
+
     }
 }
